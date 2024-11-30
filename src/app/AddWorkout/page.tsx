c14b68c44cca4dd6c72b518127b3f3e8f8cd5e35
philbreunig <phillip.breunig04@gmail.com>
2024-11-30 15:52:15 -0500

    Push #2

--- src/app/AddWorkout/page.tsx
+++ src/app/AddWorkout/page.tsx
@@ -76,7 +76,7 @@ export default function Home({ params }: { params: { username: string } }) {
       if (!response.ok) {
         throw new Error("Failed to add workout");
       }
-
+      /* TRYING TO ADD TO USER ARRAY ON SUBMIT
       const savedWorkout = await response.json();
 
       const addToUserResponse = await fetch(
@@ -98,7 +98,7 @@ export default function Home({ params }: { params: { username: string } }) {
 
       const data = await addToUserResponse.json();
       setWorkouts([...workouts, savedWorkout]);
-      router.push("/");
+      router.push("/"); */
     } catch (err) {
       console.error("Error adding workout to user:", err);
     }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,7 +92,6 @@ export default function Home() {
     logout();
   };
 
-  console.log(workouts);
   return (
     <div>
       {!isLoggedIn && (
